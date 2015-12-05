a3ad0c31e67a53bdd2af22ddf707f1744119553d
mOhamEd aTef <mOhamEd aTef@elbeh-PC>
2015-12-05 22:21:41 +0200

    :D not this

--- JDBC/src/eg/edu/alexu/csd/oop/jdbc/DriverImp.java
+++ JDBC/src/eg/edu/alexu/csd/oop/jdbc/DriverImp.java
@@ -11,49 +11,49 @@ import java.util.logging.Logger;
 
 public class DriverImp implements Driver {
 
-  @Override
-  public boolean acceptsURL(String arg0) throws SQLException {
-    return true;
-  }
+	@Override
+	public boolean acceptsURL(String arg0) throws SQLException {
+		return true;
+	}
 
-  @Override
-  public Connection connect(String url, Properties info) throws SQLException {
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+	  
+	  File dir = (File) info.get("path");
+	  String path = dir.getAbsolutePath();
+		Connection connect = new ConnectionImp(path);
+	  return connect;
+	}
 
-    // File dir = (File) info.get("path");
-    String path = ((String) info.get("path")).toLowerCase();
-    Connection connect = new ConnectionImp(path);
-    return connect;
-  }
+	@Override
+	public int getMajorVersion() {
 
-  @Override
-  public int getMajorVersion() {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public int getMinorVersion() {
 
-  @Override
-  public int getMinorVersion() {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
 
-  @Override
-  public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String arg0, Properties arg1) throws SQLException {
+	  DriverPropertyInfo[] usedMap = new DriverPropertyInfo[1];
+	  usedMap[0] = new DriverPropertyInfo("path", " ");
+		return usedMap;
+	}
 
-  @Override
-  public DriverPropertyInfo[] getPropertyInfo(String arg0, Properties arg1) throws SQLException {
-    DriverPropertyInfo[] usedMap = new DriverPropertyInfo[1];
-    usedMap[0] = new DriverPropertyInfo("path", " ");
-    return usedMap;
-  }
+	@Override
+	public boolean jdbcCompliant() {
 
-  @Override
-  public boolean jdbcCompliant() {
-
-    throw new java.lang.UnsupportedOperationException();
-  }
+		throw new java.lang.UnsupportedOperationException();
+	}
 
 }
