dfb44fa08f3356895d134f869fb51725ea64f097
mOhamEd aTef <mOhamEd aTef@elbeh-PC>
2015-12-10 23:46:14 +0200

    back to the old version for the tests to be passed if the test is run

--- JDBC/src/eg/edu/alexu/csd/oop/jdbc/MyDriver.java
+++ JDBC/src/eg/edu/alexu/csd/oop/jdbc/MyDriver.java
@@ -9,58 +9,51 @@ import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
 import java.util.logging.Logger;
 
-import eg.edu.alexu.csd.oop.jdbc.connection.handler.ConnectionGetter;
-
 public class MyDriver implements Driver {
-  ConnectionGetter connectionGetter;
 
-  public MyDriver() {
-    connectionGetter = ConnectionGetter.getInstance();
-  }
+	@Override
+	// Retrieves whether the driver thinks that it can open a connection to the
+	// given URL.
+	public boolean acceptsURL(String url) throws SQLException {
+		return true;
+	}
 
-  @Override
-  // Retrieves whether the driver thinks that it can open a connection to the
-  // given URL.
-  public boolean acceptsURL(String url) throws SQLException {
-    return true;
-  }
+	@Override
+	// Gets information about the possible properties for this driver.
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
+			throws SQLException {
+		DriverPropertyInfo[] usedMap = new DriverPropertyInfo[1];
+		usedMap[0] = new DriverPropertyInfo("path", " ");
+		return usedMap;
+	}
 
-  @Override
-  // Gets information about the possible properties for this driver.
-  public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
-    DriverPropertyInfo[] usedMap = new DriverPropertyInfo[1];
-    usedMap[0] = new DriverPropertyInfo("path", " ");
-    return usedMap;
-  }
+	@Override
+	// Attempts to make a database connection to the given URL.
+	public Connection connect(String url, Properties info) throws SQLException {
+		File dir = (File) info.get("path");
+		String path = dir.getAbsolutePath();
+		Connection connect = new MyConnection(path);
+		return connect;
+	}
 
-  @Override
-  // Attempts to make a database connection to the given URL.
-  public Connection connect(String url, Properties info) throws SQLException {
-    File dir = (File) info.get("path");
-    String path = dir.getAbsolutePath();
-    return connectionGetter.getConnection(path);// connectionGetter is
-                                                // responsible for dealing with
-                                                // the connection pool
-  }
+	@Override
+	public int getMajorVersion() {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-  @Override
-  public int getMajorVersion() {
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public int getMinorVersion() {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-  @Override
-  public int getMinorVersion() {
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
-  @Override
-  public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-    throw new java.lang.UnsupportedOperationException();
-  }
-
-  @Override
-  public boolean jdbcCompliant() {
-    throw new java.lang.UnsupportedOperationException();
-  }
+	@Override
+	public boolean jdbcCompliant() {
+		throw new java.lang.UnsupportedOperationException();
+	}
 
 }
