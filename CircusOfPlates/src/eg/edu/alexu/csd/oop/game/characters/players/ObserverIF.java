04dff07b54b49d2f70bbcafdd16555c1c2c6122f
Mohamed Sabra <mohamed2006fs@gmail.com>
2016-03-16 18:50:05 +0200

    Clean The Master Branch

--- CircusOfPlates/src/eg/edu/alexu/csd/oop/game/characters/players/ObserverIF.java
+++
@@ -1,7 +0,0 @@
-package eg.edu.alexu.csd.oop.game.characters.players;
-
-public interface ObserverIF{
-
-	public void notify(int event);
-	
-}
--- CircusOfPlates/src/eg/edu/alexu/csd/oop/game/world/MomentoIf.java
+++
@@ -1,5 +0,0 @@
-package eg.edu.alexu.csd.oop.game.world;
-
-public interface MomentoIf {
-
-}
