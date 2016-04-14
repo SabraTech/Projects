043e80487249c4a03e2a97f3a3b393456c77d80e
Mohamed Sabra <mohamed2006fs@gmail.com>
2016-04-14 15:54:52 +0200

    dictionary

--- Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/avl/Dictionary.java
+++ Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/avl/Dictionary.java
@@ -1,43 +1,79 @@
 package eg.edu.alexu.csd.filestructure.avl;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Dictionary implements IDictionary {
+  
+  private AVLTree<String> tree;
+  private int size;
+  public Dictionary(){
+    tree = new AVLTree();
+    size = 0;
+  }
 
   @Override
   public void load(File file) {
     
+    try (Scanner scanner = new Scanner(file)) {
+
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        tree.insert(line);
+        size++;
+      }
+
+      scanner.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     
   }
 
   @Override
   public boolean insert(String word) {
-    
-    return false;
+    if(tree.search(word)){
+      return false;
+    }else{
+      tree.insert(word);
+      size++;
+      return true;
+    }
   }
 
   @Override
   public boolean exists(String word) {
     
-    return false;
+    if(tree.search(word)){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   @Override
   public boolean delete(String word) {
     
-    return false;
+    if(tree.delete(word)){
+      size--;
+      return true;
+    }else{
+      return false;
+    }
   }
 
   @Override
   public int size() {
     
-    return 0;
+    return size;
   }
 
   @Override
   public int height() {
     
-    return 0;
+    return tree.height();
   }
   
 }
