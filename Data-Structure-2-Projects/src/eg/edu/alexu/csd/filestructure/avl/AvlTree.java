d480106f7a03e3368297139e358ce4d3b3728554
Mohamed Sabra <mohamed2006fs@gmail.com>
2016-04-16 17:19:59 +0200

    check style

--- Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/avl/AvlTree.java
+++ Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/avl/AvlTree.java
@@ -1,135 +1,184 @@
 package eg.edu.alexu.csd.filestructure.avl;
 
+/**
+ * The Class AvlTree.
+ *
+ * @param <T>
+ *          the generic type
+ */
 public class AvlTree<T extends Comparable<T>> implements IAVLTree<T> {
-
+  
+  /** The root. */
   private AVLNode root;
-
+  
+  /**
+   * Instantiates a new avl tree.
+   */
   public AvlTree() {
     root = null;
   }
-
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * eg.edu.alexu.csd.filestructure.avl.IAVLTree#insert(java.lang.Comparable)
+   */
   @Override
   public void insert(T key) {
     root = insertAvl(key, root);
   }
-
+  
+  /**
+   * Insert avl.
+   *
+   * @param key
+   *          the key
+   * @param root2
+   *          the root2
+   * @return the AVL node
+   */
   private AVLNode insertAvl(T key, AVLNode root2) {
     if (root2 == null) {
       return new AVLNode(key);
     }
     int compareResult = key.compareTo(root2.getValue());
-
+    
     if (compareResult < 0) {
       root2.left = insertAvl(key, root2.left);
     } else {
       root2.right = insertAvl(key, root2.right);
     }
-
-    root2.height = (Math.max(getHeight(root2.left), getHeight(root2.right)) + 1);
-
+    
+    root2.height = (Math.max(getHeight(root2.left), getHeight(root2.right))
+        + 1);
+    
     int balance = getBalance(root2);
-
+    
     // left left case
     if (balance > 1 && key.compareTo(root2.left.getValue()) < 0) {
       return rotateRight(root2);
     }
-
+    
     // right right case
     if (balance < -1 && key.compareTo(root2.right.getValue()) > 0) {
       return rotateLeft(root2);
     }
-
+    
     // left right case
     if (balance > 1 && key.compareTo(root2.left.getValue()) > 0) {
       root2.left = rotateLeft(root2.left);
       return rotateRight(root2);
     }
-
+    
     // right left case
     if (balance < -1 && key.compareTo(root2.right.getValue()) < 0) {
       root2.right = rotateRight(root2.right);
       return rotateLeft(root2);
     }
     return root2;
-
+    
   }
-
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * eg.edu.alexu.csd.filestructure.avl.IAVLTree#delete(java.lang.Comparable)
+   */
   @Override
   public boolean delete(T key) {
-
+    
     if (search(key)) {
       root = deleteAvl(key, root);
       return true;
     } else {
       return false;
     }
-
+    
   }
-
+  
+  /**
+   * Delete avl.
+   *
+   * @param key
+   *          the key
+   * @param root2
+   *          the root2
+   * @return the AVL node
+   */
   private AVLNode deleteAvl(T key, AVLNode root2) {
     if (root2 == null) {
       return root2;
     }
-
+    
     int compareResult = key.compareTo(root2.getValue());
-
+    
     if (compareResult < 0) {
       root2.setLeftChild(deleteAvl(key, root2.left));
     } else if (compareResult > 0) {
       root2.setRightChild(deleteAvl(key, root2.right));
     } else {
-        
-        if ((root2.left == null) || (root2.right == null)) {
-          AVLNode temp = null;
-          if (temp == root2.left) {
-            temp = root2.right;
-          } else {
-            temp = root2.left;
-          }
-
-          if (temp == null) {
-            temp = root2;
-            root2 = null;
-          } else {
-            root2 = temp;
-          }
+      
+      if ((root2.left == null) || (root2.right == null)) {
+        AVLNode temp = null;
+        if (temp == root2.left) {
+          temp = root2.right;
         } else {
-
-          AVLNode temp = getMin(root2.right);
-          root2.setValue(temp.getValue());
-          root2.setRightChild(deleteAvl(temp.getValue(), root2.right));
+          temp = root2.left;
         }
+        
+        if (temp == null) {
+          temp = root2;
+          root2 = null;
+        } else {
+          root2 = temp;
+        }
+      } else {
+        
+        AVLNode temp = getMin(root2.right);
+        root2.setValue(temp.getValue());
+        root2.setRightChild(deleteAvl(temp.getValue(), root2.right));
       }
-
-      if (root2 == null) {
-        return root2;
-      }
-
-      root2.setHeight(Math.max(getHeight(root2.left), getHeight(root2.right)) + 1);
-
-      int balance = getBalance(root2);
-
-      if (balance > 1 && getBalance(root2.left) >= 0) {
-        return rotateRight(root2);
-      }
-
-      if (balance > 1 && getBalance(root2.left) < 0) {
-        root2.setLeftChild(rotateLeft(root2.left));
-        return rotateRight(root2);
-      }
-
-      if (balance < -1 && getBalance(root2.right) <= 0) {
-        return rotateLeft(root2);
-      }
-
-      if (balance < -1 && getBalance(root2.right) > 0) {
-        root2.setRightChild(rotateRight(root2.right));
-        return rotateLeft(root2);
-      }
-
+    }
+    
+    if (root2 == null) {
       return root2;
     }
-
+    
+    root2
+        .setHeight(Math.max(getHeight(root2.left), getHeight(root2.right)) + 1);
+    
+    int balance = getBalance(root2);
+    
+    if (balance > 1 && getBalance(root2.left) >= 0) {
+      return rotateRight(root2);
+    }
+    
+    if (balance > 1 && getBalance(root2.left) < 0) {
+      root2.setLeftChild(rotateLeft(root2.left));
+      return rotateRight(root2);
+    }
+    
+    if (balance < -1 && getBalance(root2.right) <= 0) {
+      return rotateLeft(root2);
+    }
+    
+    if (balance < -1 && getBalance(root2.right) > 0) {
+      root2.setRightChild(rotateRight(root2.right));
+      return rotateLeft(root2);
+    }
+    
+    return root2;
+  }
+  
+  /**
+   * Gets the min.
+   *
+   * @param node
+   *          the node
+   * @return the min
+   */
   private AVLNode getMin(AVLNode node) {
     AVLNode current = node;
     while (current.left != null) {
@@ -137,7 +186,13 @@ public class AvlTree<T extends Comparable<T>> implements IAVLTree<T> {
     }
     return current;
   }
-
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * eg.edu.alexu.csd.filestructure.avl.IAVLTree#search(java.lang.Comparable)
+   */
   @Override
   public boolean search(T key) {
     AVLNode temp = root;
@@ -151,7 +206,12 @@ public class AvlTree<T extends Comparable<T>> implements IAVLTree<T> {
     }
     return false;
   }
-
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see eg.edu.alexu.csd.filestructure.avl.IAVLTree#height()
+   */
   @Override
   public int height() {
     if (root == null) {
@@ -159,101 +219,195 @@ public class AvlTree<T extends Comparable<T>> implements IAVLTree<T> {
     }
     return root.getHeight();
   }
-
+  
+  /*
+   * (non-Javadoc)
+   * 
+   * @see eg.edu.alexu.csd.filestructure.avl.IAVLTree#getTree()
+   */
   @Override
   public INode<T> getTree() {
-    return (INode) root;
+    return (INode<T>) root;
   }
-
+  
+  /**
+   * Gets the height.
+   *
+   * @param node
+   *          the node
+   * @return the height
+   */
   private int getHeight(AVLNode node) {
     if (node == null) {
       return 0;
     }
     return node.getHeight();
   }
-
+  
+  /**
+   * Gets the balance.
+   *
+   * @param node
+   *          the node
+   * @return the balance
+   */
   private int getBalance(AVLNode node) {
     if (node == null) {
       return 0;
     }
     return getHeight(node.left) - getHeight(node.right);
   }
-
+  
+  /**
+   * Rotate right.
+   *
+   * @param y
+   *          the y
+   * @return the AVL node
+   */
   private AVLNode rotateRight(AVLNode y) {
     AVLNode x = y.left;
     AVLNode t2 = x.right;
-
+    
     x.setRightChild(y);
     y.setLeftChild(t2);
-
+    
     y.setHeight((Math.max(getHeight(y.left), getHeight(y.right)) + 1));
-    x.setHeight( (Math.max(getHeight(x.left), getHeight(x.right)) + 1) );
-
+    x.setHeight((Math.max(getHeight(x.left), getHeight(x.right)) + 1));
+    
     return x;
   }
-
+  
+  /**
+   * Rotate left.
+   *
+   * @param x
+   *          the x
+   * @return the AVL node
+   */
   private AVLNode rotateLeft(AVLNode x) {
     AVLNode y = x.right;
     AVLNode t2 = y.left;
-
+    
     y.setLeftChild(x);
     x.setRightChild(t2);
-
+    
     x.setHeight((Math.max(getHeight(x.left), getHeight(x.right)) + 1));
     y.setHeight((Math.max(getHeight(y.left), getHeight(y.right)) + 1));
-
+    
     return y;
   }
-
+  
+  /**
+   * The Class AVLNode.
+   */
   private class AVLNode implements INode<T> {
+    
+    /** The element. */
     private T element;
+    
+    /** The left. */
     private AVLNode left;
+    
+    /** The right. */
     private AVLNode right;
+    
+    /** The height. */
     private int height;
-
+    
+    /**
+     * Instantiates a new AVL node.
+     *
+     * @param key
+     *          the key
+     */
     public AVLNode(T key) {
       this.element = key;
       this.height = 1;
     }
-
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see eg.edu.alexu.csd.filestructure.avl.INode#getLeftChild()
+     */
     @Override
     public INode<T> getLeftChild() {
       return this.left;
     }
-
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see eg.edu.alexu.csd.filestructure.avl.INode#getRightChild()
+     */
     @Override
     public INode<T> getRightChild() {
       return this.right;
     }
-
+    
+    /**
+     * Sets the left child.
+     *
+     * @param left
+     *          the new left child
+     */
     public void setLeftChild(AVLNode left) {
       this.left = left;
     }
-
+    
+    /**
+     * Sets the right child.
+     *
+     * @param right
+     *          the new right child
+     */
     public void setRightChild(AVLNode right) {
       this.right = right;
     }
-
+    
+    /**
+     * Sets the height.
+     *
+     * @param height
+     *          the new height
+     */
     public void setHeight(int height) {
       this.height = height;
     }
-
+    
+    /**
+     * Gets the height.
+     *
+     * @return the height
+     */
     public int getHeight() {
       return this.height;
-
+      
     }
-
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see eg.edu.alexu.csd.filestructure.avl.INode#getValue()
+     */
     @Override
     public T getValue() {
       return this.element;
     }
-
+    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * eg.edu.alexu.csd.filestructure.avl.INode#setValue(java.lang.Comparable)
+     */
     @Override
     public void setValue(T value) {
       this.element = value;
-
+      
     }
-
+    
   }
-
+  
 }
