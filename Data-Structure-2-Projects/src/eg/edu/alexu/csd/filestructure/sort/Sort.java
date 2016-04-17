6483d1fafc216ff347098c388ba2412f1d6c745a
Mohamed Sabra <mohamed2006fs@gmail.com>
2016-04-18 00:37:35 +0200

    last style ++

--- Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/sort/Sort.java
+++ Data-Structure-2-Projects/src/eg/edu/alexu/csd/filestructure/sort/Sort.java
@@ -9,18 +9,19 @@ import java.util.ArrayList;
  *          the generic type
  */
 public class Sort<T extends Comparable<T>> implements ISort<T> {
-  
+
   /*
    * (non-Javadoc)
-   * 
-   * @see eg.edu.alexu.csd.filestructure.sort .ISort#heapSort(java.util.ArrayList)
+   *
+   * @see eg.edu.alexu.csd.filestructure.sort
+   * .ISort#heapSort(java.util.ArrayList)
    */
   @Override
   public IHeap<T> heapSort(final ArrayList<T> unordered) {
     if (unordered == null || unordered.size() == 0) {
       throw new RuntimeException("Array is null or size is zero");
     }
-    
+
     Heap<T> myHeap = new Heap<T>();
     ArrayList<T> orderedArray = new ArrayList<T>();
     myHeap.build(unordered);
@@ -29,13 +30,14 @@ public class Sort<T extends Comparable<T>> implements ISort<T> {
     }
     myHeap.setSize(unordered.size());
     return myHeap;
-    
+
   }
-  
+
   /*
    * (non-Javadoc)
-   * 
-   * @see eg.edu.alexu.csd.filestructure.sort .ISort#sortSlow(java.util.ArrayList)
+   *
+   * @see eg.edu.alexu.csd.filestructure.sort
+   * .ISort#sortSlow(java.util.ArrayList)
    */
   @Override
   public void sortSlow(final ArrayList<T> unordered) {
@@ -45,31 +47,32 @@ public class Sort<T extends Comparable<T>> implements ISort<T> {
     for (int i = 0; i < unordered.size(); i++) {
       for (int j = 0; j < unordered.size() - 1; j++) {
         if (unordered.get(j).compareTo(unordered.get(j + 1)) == 1) {
-          
+
           T tmp = unordered.get(j);
           unordered.set(j, unordered.get(j + 1));
           unordered.set(j + 1, tmp);
-          
+
         }
       }
     }
   }
-  
+
   /*
    * (non-Javadoc)
-   * 
-   * @see eg.edu.alexu.csd.filestructure.sort .ISort#sortFast(java.util.ArrayList)
+   *
+   * @see eg.edu.alexu.csd.filestructure.sort
+   * .ISort#sortFast(java.util.ArrayList)
    */
   @Override
   public void sortFast(final ArrayList<T> unordered) {
-    
+
     if (unordered == null || unordered.size() == 0) {
       throw new RuntimeException("Array is null or size is zero");
     }
     quickSort(unordered, 0, unordered.size() - 1);
-    
+
   }
-  
+
   /**
    * Quick sort.
    *
@@ -82,26 +85,26 @@ public class Sort<T extends Comparable<T>> implements ISort<T> {
    */
   private void quickSort(final ArrayList<T> unordered,
       final int lowerIndex, final int upperIndex) {
-    
+
     if (lowerIndex >= upperIndex) {
       return;
     }
-    
+
     int lower = lowerIndex;
     int upper = upperIndex;
     // calculate the pivot
     T pivot = unordered.get(lower + (upper - lower) / 2);
-    
+
     // divide into two arrays
     while (lower <= upper) {
       while ((unordered.get(lower).compareTo(pivot)) < 0) {
         lower++;
       }
-      
+
       while ((unordered.get(upper).compareTo(pivot)) > 0) {
         upper--;
       }
-      
+
       if (lower <= upper) {
         T tmp = unordered.get(lower);
         unordered.set(lower, unordered.get(upper));
@@ -110,14 +113,14 @@ public class Sort<T extends Comparable<T>> implements ISort<T> {
         upper--;
       }
     }
-    
+
     if (lowerIndex < upper) {
       quickSort(unordered, lowerIndex, upper);
     }
     if (lower < upperIndex) {
       quickSort(unordered, lower, upperIndex);
     }
-    
+
   }
-  
+
 }
